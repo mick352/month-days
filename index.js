51e2b70e82bc4f8502433f06303acae82232d349
mick352 <54581004+mick352@users.noreply.github.com>
2019-10-14 13:19:31 +0200

    add type checking to isDefined

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 'use strict';
-const isDefined = value => value !== undefined;
+const isDefined = value => typeof(value) !== "undefined";
 
 module.exports = ({month, year} = {}) => {
 	if (isDefined(month) && typeof month !== 'number') {
